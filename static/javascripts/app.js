fa3650ff7d778f6699645a05d766db07d31e1c4d
buddhadev04 <buddhadevdas237@gmail.com>
2024-06-12 13:19:45 +0530

    added the functionality of show password

--- static/javascripts/app.js
+++ static/javascripts/app.js
@@ -5,16 +5,28 @@ const sign_in_btn2 = document.querySelector("#sign-in-btn2");
 const sign_up_btn2 = document.querySelector("#sign-up-btn2");
 
 sign_up_btn.addEventListener("click", () => {
-    container.classList.add("sign-up-mode");
+  container.classList.add("sign-up-mode");
 });
 
 sign_in_btn.addEventListener("click", () => {
-    container.classList.remove("sign-up-mode");
+  container.classList.remove("sign-up-mode");
 });
 
 sign_up_btn2.addEventListener("click", () => {
-    container.classList.add("sign-up-mode2");
+  container.classList.add("sign-up-mode2");
 });
 sign_in_btn2.addEventListener("click", () => {
-    container.classList.remove("sign-up-mode2");
+  container.classList.remove("sign-up-mode2");
+});
+
+// for password hide and show
+document.querySelectorAll(".eye-icon").forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", function () {
+    const passwordInput = this.previousElementSibling;
+    const type =
+      passwordInput.getAttribute("type") === "password" ? "text" : "password";
+    passwordInput.setAttribute("type", type);
+    this.classList.toggle("fa-eye");
+    this.classList.toggle("fa-eye-slash");
+  });
 });
